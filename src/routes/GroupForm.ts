538ba25ad743a3bcb365ec5d6b948eef17393703
luzone <eyal.luzon@hpe.com>
2018-03-22 11:19:05 +0200

    add student history

--- src/routes/GroupForm.ts
+++ src/routes/GroupForm.ts
@@ -2,8 +2,6 @@
 import express, { Request, Response } from 'express';
 import http from 'http';
 
-const mock = require('../../db/db.json').students;
-
 class GroupFormRoute {
   public express: express.Router;
 
@@ -15,7 +13,16 @@ class GroupFormRoute {
   private mountRoutes(): void {
     this.express.post('', (req: Request, res: Response) => {
       const query: IGrouperQuery = req.body.groupQuery;
-
+      // 1. get the students by calling the students endpoint (db server)
+      //  http://localhost:3004/students?_embed=studentHistory
+        const students = [];
+      // 2. reduce the history to a count attribute on students
+      const studentMappings = {};
+        students.map((student: IStudent) => {
+        studentMappings[student.id] = [];
+      });
+      // 3. pass the aggregated data to the python app server
+      // 4. return the result to the caller (ui server)
       const requestOptions: http.RequestOptions = {
         host: 'localhost',
         method: 'POST',
