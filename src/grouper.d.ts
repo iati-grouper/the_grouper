c9615b611417870ae2439202d9f0e806513c682e
luzone <eyal.luzon@hpe.com>
2018-03-22 13:41:43 +0200

    add gender, level. initial call to mapped resource

--- src/grouper.d.ts
+++ src/grouper.d.ts
@@ -2,6 +2,9 @@ interface IStudent {
   id: number;
   name: string;
   imageUrl: string;
+  gender?: string;
+  level?: number;
+  studentHistory?: IStudentHistory[];
 }
 
 interface IGrouperQuery {
@@ -11,4 +14,9 @@ interface IGrouperQuery {
 
 interface IGroupParameters {
   size: number;
+}
+
+interface IStudentHistory {
+    group: number[];
+    studentId: string;
 }
--- src/routes/GroupForm.ts
+++ src/routes/GroupForm.ts
@@ -12,32 +12,45 @@ class GroupFormRoute {
 
   private mountRoutes(): void {
     this.express.post('', (req: Request, res: Response) => {
-      const query: IGrouperQuery = req.body.groupQuery;
-      // 1. get the students by calling the students endpoint (db server)
-      //  http://localhost:3004/students?_embed=studentHistory
-        const students = [];
-      // 2. reduce the history to a count attribute on students
-      const studentMappings = {};
-        students.map((student: IStudent) => {
-        studentMappings[student.id] = [];
-      });
-      // 3. pass the aggregated data to the python app server
-      // 4. return the result to the caller (ui server)
-      const requestOptions: http.RequestOptions = {
-        host: 'localhost',
-        method: 'POST',
-        path: '/grouper',
-        port: 5000,
-      };
-
-      const request: http.ClientRequest = http.request(requestOptions, (response: http.ClientResponse) => {
-        response.on('data', (chunk: any) => {
-          res.send(chunk.toString());
+        // 1. get the students by calling the students endpoint (db server)
+        //  http://localhost:3004/students?_embed=studentHistory
+        const query: IGrouperQuery = req.body.groupQuery;
+        const studentMappings = {};
+        http.get({
+            agent: false,  // create a new agent just for this one request
+            hostname: 'localhost',
+            path: '/students?_embed=studentHistory',
+            port: 3004,
+        }, (response: any) => {
+            response.on('data', (chunk: any) => {
+                const students = JSON.parse(chunk.toString());
+                console.log("students", students);
+                res.send(chunk.toString());
+                students.map( (student: IStudent) => {
+                    studentMappings[student.id] = {
+                        student
+                    };
+                });
+            });
         });
-      });
-
-      request.write(JSON.stringify(query));
-      request.end();
+      // 2. reduce the history to a count attribute on students
+      // 3. pass the aggregated data to the python app server
+      // const requestOptions: http.RequestOptions = {
+      //   host: 'localhost',
+      //   method: 'POST',
+      //   path: '/grouper',
+      //   port: 5000,
+      // };
+      //
+      // const request: http.ClientRequest = http.request(requestOptions, (response: http.ClientResponse) => {
+      //   response.on('data', (chunk: any) => {
+      //       // 4. return the result to the caller (ui server)
+      //     res.send(chunk.toString());
+      //   });
+      // });
+      //
+      // request.write(JSON.stringify(query));
+      // request.end();
     });
   }
 }
